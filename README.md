5444b53aaddd7804f7f7bb32065e774fe4c7a14f
Tyson Andre <tysonandre775@hotmail.com>
2022-10-24 22:30:20 -0400

    Add an example benchmark.

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 immutable\_cache
 ================
 
-**Not working yet**
+**This is currently a prototype. Error handling and API design will likely change in subsequent releases. There may be bugs.**
+
 This is a work in progress PECL repo adding functionality similar to APCu,
 but with immutable values (an immutable serialized copy of mutable values is stored in the serializer)
 
@@ -17,16 +18,84 @@ Goals:
 Benefits:
 
 - Can be certain that a value added to the cache doesn't get removed
-- Once implemented, can efficiently load entire immutable arrays or strings in cases where the serializer is not required.
+- Can efficiently fetch entire immutable arrays or strings without copying or using extra memory
+  in cases where the serializer is not required.
+
+  (apcu needs to make copies of all strings and arrays in case of a cache clear during a request)
 
 Features
 ========
 
-ImmutableCache is an in-memory key-value store for PHP. Keys are of type string and values can be any PHP variables.
+`immutable_cache` is an in-memory key-value store for PHP. Keys are of type string and values can be any PHP variables.
 
-APCu only supports userland caching of immutable representations of variables.
+`immutable_cache` only supports userland caching of immutable representations of keys.
 
 See [`php_immutable_cache.stub.php`](./php_immutable_cache.stub.php) for the api.
+This provides an API similar to a subset of https://php.net/apcu , but does not allow for modifying a created key.
+
+### Benefits
+
+Immutability allows `immutable_cache` to keep strings and arrays in shared memory, without making a copy.
+Different processes (e.g. from apache worker pools) or threads (in ZTS builds) would
+all be performing read-only access to the same constant arrays (like they would with opcache)
+
+### Benchmarks
+
+Note that when the data is completely immutable (i.e. does not need to call the php serializer for anything),
+it can be stored in shared memory and retrieved from
+`immutable_cache` without any modification (e.g. very large arrays, large strings, etc),
+and would have the same throughput.
+
+```
+E.g. to retrieve multiple versions of the fake cached config
+{"key0":"myValue0","key1":"myValue1","key2":"myValue2","key3":"myValue3","key4":"myValue4","key5":"myValue5","key6":"myValue6","key7":"myValue7"}
+as a php array with 4 processes, repeatedly (with immutable_cache.enable_cli=1, immutable_cache.enabled=1, etc).
+
+APCu            Elapsed: 0.339511 throughput    1178166 / second
+APCu            Elapsed: 0.342087 throughput    1169294 / second
+APCu            Elapsed: 0.424223 throughput     942901 / second
+APCu            Elapsed: 0.424893 throughput     941413 / second
+immutable_cache Elapsed: 0.109808 throughput    3642719 / second
+immutable_cache Elapsed: 0.117099 throughput    3415902 / second
+immutable_cache Elapsed: 0.099313 throughput    4027680 / second
+immutable_cache Elapsed: 0.105851 throughput    3778904 / second
+```
+
+### Ini settings
+
+Similar to https://www.php.net/manual/en/apcu.configuration.php
+
+- `immutable_cache.enabled` (bool, defaults to 1(on))
+- `immutable_cache.enable_cli` (bool, defaults to 0(off))
+- `immutable_cache.shm_size`
+- `immutable_cache.entries_hint`
+- `immutable_cache.shm_size`
+- `immutable_cache.serializer` (currently `php` or `default`. Defaults to `php`. This is used to serialize data that doesn't have an immutable representation (e.g. objects, references (for now, it isn't converted)))
+
+This is an immutable cache where entries can't be removed, so there is no need for ttl or gc_ttl.
+
+Installing
+==========
+
+## Compile immutable_cache in Linux
+
+```
+$ phpize
+$ ./configure
+$ make
+$ make test
+$ make install
+```
+
+Add the following line to your php.ini
+
+```
+extension=immutable_cache.so
+```
+
+## Compile immutable_cache in Windows
+
+See https://wiki.php.net/internals/windows/stepbystepbuild_sdk_2#building_pecl_extensions and .appveyor.yml for how to build this.
 
 Reporting Bugs
 =============
--- benchmark_shm.php
+++ benchmark_shm.php
@@ -2,67 +2,53 @@
 
 function test_apcu(int $n, $value) {
     for ($i = 0; $i < 16; $i++) {
-        apcu_store('key' . $i, $value);
+        if (!apcu_store('key' . $i, $value)) {
+            var_export(apcu_sma_info());
+            throw new RuntimeException("Failed to store value\n");
+        }
     }
-    if (!apcu_store('key', $value)) {
-        var_export(apcu_sma_info());
-        throw new RuntimeException("Failed to store value\n");
-    }
-    $ser = serialize($value);
     $start = hrtime(true);
     for ($i = 0; $i < $n; $i++) {
-
-        // apcu_store throughput empty new stdClass()
-        //   100000 per second with 8  processes (4 real cores with 2 threads - effectively 4 since all of this is CPU-bound)
-        //   230000 per second with 4  processes
-        //   490000 per second with 2  processes
-        //  3070000 per second with 1  process
-        //apcu_store('key', $value);  // using different keys doesn't have much difference
-
-        // unserialize only, no apcu (cpu benchmarking only)
-        // 1420000 per second with 8 processes
-        // 3080000 per second with 4 processes
-        // 3680000 per second with 2 processes
-        // 3770000 per second with 2 processes
-        //unserialize($ser);
-
-        // apcu_fetch throughput empty new stdClass(), default serializer
-        //   350000 per second with 32
-        //   860000 per second with 8 processes (97 no lock)
-        //  1290000 per second with 4 processes (145 no lock, 170 no stats, 190 separate keys)
-        //  1760000 per second with 2 processes (230 no lock)
-        //  2660000 per second with 1 process   (260 no lock)
-
-        // apcu_fetch throughput value=42, storing raw value
-        //  1200000 per second with 8 processes
-        //  1790000 per second with 4 processes (4100000 no lock)
-        //  3000000 per second with 2 processes
-        //  9850000 per second with 1 process
-        //  I guess there's a bottleneck of 10 million apcu fetches per second. Can we hit that with a 128-core machine?
-        //
-        // This can be avoided if you run multiple apache server pools.
         apcu_fetch('key' . ($i & 15), $value);
     }
     $end = hrtime(true);
     $elapsed = ($end - $start)/1e9;
-    printf("APCu Elapsed: %.6f throughput %10.0f\n", $elapsed,  (1/$elapsed * $n));
-    //printf("count=%d\n", count((array)apcu_fetch('key', $value)));
+    printf("APCu            Elapsed: %.6f throughput %10.0f / second\n", $elapsed,  (1/$elapsed * $n));
+    // printf("count=%d\n", count((array)immutable_cache_fetch('key0', $value)));
 }
-const N = 4000000;
+function test_immutablecache(int $n, $value) {
+    for ($i = 0; $i < 16; $i++) {
+        if (!immutable_cache_add('imm' . $i, $value)) {
+            // expected, since there are multiple processes from pcntl and only the first will succeed
+        }
+    }
+    $start = hrtime(true);
+    for ($i = 0; $i < $n; $i++) {
+        immutable_cache_fetch('key' . ($i & 15), $value);
+    }
+    $end = hrtime(true);
+    $elapsed = ($end - $start)/1e9;
+    printf("immutable_cache Elapsed: %.6f throughput %10.0f / second\n", $elapsed,  (1/$elapsed * $n));
+    // printf("count=%d\n", count((array)apcu_fetch('key0', $value)));
+}
+const N = 400000;
 const NUM_PROCESSES = 4;
-$value = 42;//new stdClass();
 
-printf("Testing with %d processes\n", NUM_PROCESSES);
+$value = [];
+for ($i = 0; $i < 8; $i++) {
+    $value["key$i"] = "myValue$i";
+}
+
+printf("Testing string->string array of size 8 with %d processes\n", NUM_PROCESSES);
+echo "Note that the immutable array and strings part of shared memory in immutable_cache, where apcu must make copies of strings and arrays to account for eviction (and php must free them)\n";
+echo json_encode($value), "\n\n";
+
 $pids = [];
 for ($i = 0; $i < NUM_PROCESSES; $i++) {
     $result = pcntl_fork();
     if ($result <= 0) {
-        //ob_start();
-        // $outname = "out-" . getmypid() . ".out";
-        //file_put_contents($outname, ob_get_clean());
-        //test_shm(N, new stdClass());
         test_apcu(N, $value);
-        //file_put_contents($outname, ob_get_clean());
+        test_immutablecache(N, $value);
         exit(0);
     } else {
         $pids[] = $result;
@@ -73,3 +59,17 @@ foreach ($pids as $pid) {
     pcntl_waitpid($pid, $status);
     echo "status of $pid is $status\n";
 }
+/*
+E.g. to retrieve multiple versions of the fake cached config
+{"key0":"myValue0","key1":"myValue1","key2":"myValue2","key3":"myValue3","key4":"myValue4","key5":"myValue5","key6":"myValue6","key7":"myValue7"}
+as a php array with 4 processes, repeatedly (with immutable_cache.enable_cli=1, immutable_cache.enabled=1, etc).
+
+APCu            Elapsed: 0.339511 throughput    1178166 / second
+APCu            Elapsed: 0.342087 throughput    1169294 / second
+APCu            Elapsed: 0.424223 throughput     942901 / second
+APCu            Elapsed: 0.424893 throughput     941413 / second
+immutable_cache Elapsed: 0.109808 throughput    3642719 / second
+immutable_cache Elapsed: 0.117099 throughput    3415902 / second
+immutable_cache Elapsed: 0.099313 throughput    4027680 / second
+immutable_cache Elapsed: 0.105851 throughput    3778904 / second
+ */
