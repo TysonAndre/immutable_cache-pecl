72282c7649e8f6784e62235267d96f34489a03c5
Tyson Andre <tysonandre775@hotmail.com>
2022-11-12 17:13:31 -0500

    Fix test failure from loaded module

--- ci/test_inner.sh
+++ ci/test_inner.sh
@@ -6,6 +6,7 @@ set -xeu
 echo "Run tests in docker"
 php --version
 php --ini
+
 cp ci/run-tests-parallel.php run-tests.php
 export REPORT_EXIT_STATUS=1
 make test TESTS="-j$(nproc) --show-diff -d immutable_cache.serializer=default tests"
@@ -17,7 +18,7 @@ PECL_EXTENSION_DIR=$(php -r "echo ini_get('extension_dir');")
 echo "Pecl extensions are installed in $PECL_EXTENSION_DIR"
 igbinary_path="$PECL_EXTENSION_DIR/igbinary.so"
 if [[ -f "$igbinary_path" ]]; then
-	cp "$PECL_EXTENSION_DIR/igbinary.so" modules/
+	cp "$igbinary_path" modules/
 	php -d extension=modules/immutable_cache.so -d extension=modules/igbinary.so -d immutable_cache.serializer=igbinary -r 'phpinfo();'
 	make test TESTS="-j$(nproc) --show-diff -d extension=$PWD/modules/igbinary.so -d immutable_cache.serializer=igbinary tests"
 fi
